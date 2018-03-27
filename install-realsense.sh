9798aa72928ee99f3602b5bf9234ca5de686cf02
Joseph <jlefe074@uottawa.ca>
2018-03-26 21:15:28 -0400

    Added a pckage that is usually required

--- install-realsense.sh
+++ install-realsense.sh
@@ -3,6 +3,7 @@
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install libusb-1.0-0-dev pkg-config
 sudo apt-get install libglfw3-dev
+sudo apt-get install build-essential libgtk-3-dev
 
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense
